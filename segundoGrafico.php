923b36d6e3347f86a31e3e8e687521323f837ee7
Macedo <cleitonrt3@gmail.com>
2021-05-30 12:49:28 -0300

    alteração

--- segundoGrafico.php
+++ segundoGrafico.php
@@ -7,7 +7,6 @@
 <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
-
       function drawChart() {
         var data = google.visualization.arrayToDataTable([  
           ['Cidade', 'População', {role: 'style'}],
